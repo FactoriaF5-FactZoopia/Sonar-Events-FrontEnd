40f1728eae2bf751b78e3321aad037785c893b8a
Pablo-SanJose-Acedo <psanjosea@gmail.com>
2024-08-27 09:36:06 +0200

    partly done table

---
+++ src/components/Table.vue
@@ -0,0 +1,50 @@
+<template>
+<table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Title</th>
+      <th scope="col">Date</th>
+      <th scope="col">Available</th>
+      <th scope="col">Past Event</th>
+      <th scope="col">Max Participants</th>
+      <th scope="col">Description</th>
+      <th scope="col">Image</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Estopa</td>
+      <td>19 Oct 2024</td>
+      <td>Yes</td>
+      <td>No</td>
+      <td>7,500</td>
+      <td>Plaza de Toros El Bibio, Gijón</td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>The Hardkiss</td>
+      <td>26 Oct 2024</td>
+      <td>Yes</td>
+      <td>No</td>
+      <td>1,100</td>
+      <td>Patioh de la Laboral, Gijón</td>
+      <td></td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Nach</td>
+      <td>31 Oct 2024</td>
+      <td>Yes</td>
+      <td>No</td>
+      <td>500</td>
+      <td>Sir Lauren's, Oviedo</td>
+      <td></td>
+    </tr>
+  </tbody>
+</table>
+</template>
+<style>
+</style>
---
+++ src/views/AdminView.vue
@@ -0,0 +1,24 @@
+<script>
+import Table from '../components/Table.vue';
+
+export default {
+  name: 'AdminView',
+  components: {
+    Table,
+  },
+};
+</script>
+
+<template>
+  <div>
+    <h1>Admin View</h1>
+
+    <Table />
+  </div>
+</template>
+
+<style scoped>
+main {
+  height: 800px;
+}
+</style>
